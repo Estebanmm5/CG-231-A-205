acbfc75ad7768001e73eef7e55aa3883981880e2
Estebanmm5 <124269560+Estebanmm5@users.noreply.github.com>
2023-04-13 10:16:28 -0500

    Add files via upload

---
+++ ej01.js
@@ -0,0 +1,20 @@
+//Escriba la definición de una función en javascript que reciba un arreglo de elementos de tipo entero y retorne el elemento de menor valor.
+function menor(arreglo){
+    var menor = arreglo[0];
+    for(i=0; i<= arreglo.length; i++){
+        if (arreglo[i] < menor)
+        arreglo[i] = menor;
+    }
+    return menor;
+
+}
+
+//Escriba la definición de una función en javascript que reciba un vector 3D cualquiera como parámetro 
+//(interpretarlo como mejor lo considere) y retorne si el mismo es o no paralelo a uno de los ejes principales (X, Y o Z).
+
+function paralelo (vt){
+    for (i=0; i<=vt.length; i++){
+        if (vt)
+
+    }
+}
